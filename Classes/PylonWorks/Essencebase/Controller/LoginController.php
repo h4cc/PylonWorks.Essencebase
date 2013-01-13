e2e5614f335ae12364afd2ab4c4f6f297454ef57
Michael Klapper <development@morphodo.com>
2013-01-13 15:30:57 +0000

    Add some first code

--- Classes/PylonWorks/Essencebase/Controller/LoginController.php
+++ Classes/PylonWorks/Essencebase/Controller/LoginController.php
@@ -16,14 +16,58 @@ use TYPO3\Flow\Annotations as Flow;
 class LoginController extends \TYPO3\Flow\Security\Authentication\Controller\AbstractAuthenticationController {
 
 	/**
+	 * @var \TYPO3\Flow\Security\Authentication\AuthenticationManagerInterface
+	 * @Flow\Inject
+	 */
+	protected $authenticationManager;
+
+	/**
+	 * @var \TYPO3\Flow\Security\AccountRepository
+	 * @Flow\Inject
+	 */
+	protected $accountRepository;
+
+	/**
+	 * @var \TYPO3\Flow\Security\AccountFactory
+	 * @Flow\Inject
+	 */
+	protected $accountFactory;
+
+	/**
+	 * @var \TYPO3\Flow\Log\SecurityLoggerInterface
+	 * @Flow\Inject
+	 */
+	protected $securityLogger;
+
+	/**
 	 * Index action
 	 *
 	 * @return void
 	 */
 	public function indexAction() {
-		$this->view->assign('foos', array(
-			'bar', 'baz'
-		));
+
+	}
+
+	/**
+	 * @throws \TYPO3\Flow\Security\Exception\AuthenticationRequiredException
+	 * @return void
+	 */
+	public function authenticateAction() {
+
+		$this->securityLogger->log("called authenticate");
+
+		try {
+			$this->securityLogger->log("called authenticate - 2");
+			$this->authenticationManager->authenticate();
+			$this->securityLogger->log("called authenticate - 3");
+			$this->flashMessageContainer->addMessage(new \TYPO3\Flow\Error\Error('Successfully logged in.'));
+			$this->redirect('index', 'Login');
+		} catch (\TYPO3\Flow\Security\Exception\AuthenticationRequiredException $exception) {
+			$this->securityLogger->log("called authenticate - 4");
+			$this->flashMessageContainer->addMessage(new \TYPO3\Flow\Error\Error('Wrong username or password.'));
+			$this->flashMessageContainer->addMessage(new \TYPO3\Flow\Error\Error($exception->getMessage()));
+			throw $exception;
+		}
 	}
 
 	/**
@@ -43,15 +87,27 @@ class LoginController extends \TYPO3\Flow\Security\Authentication\Controller\Abs
 	 */
 	protected function onAuthenticationSuccess(\TYPO3\Flow\Mvc\ActionRequest $originalRequest = NULL) {
 		// TODO: Implement onAuthenticationSuccess() method.
+
+		$this->securityLogger->log("called onAuthenticationSuccess");
+
+	}
+
+	public function logoutAction() {
+		$this->authenticationManager->logout();
+		$this->flashMessageContainer->addMessage(new \TYPO3\Flow\Error\Error('Successfully logged out.'));
+		$this->redirect('index', 'Login');
 	}
 
 	/**
 	 *
 	 */
 	public function loginAction() {
+		$this->securityLogger->log("called loginAction");
 		if ($this->securityContext->getAccount() !== NULL) {
+			$this->securityLogger->log("called loginAction - success");
 			$this->redirect('index', 'Dashboard');
 		} else {
+			$this->securityLogger->log("called loginAction - failed, redirect back to loginAction");
 			parent::loginAction();
 		}
 	}
