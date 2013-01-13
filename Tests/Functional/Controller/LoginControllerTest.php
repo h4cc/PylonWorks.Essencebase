378b5d5be33e6f70ba3851547effe6dfb76971ac
Michael Klapper <development@morphodo.com>
2013-01-13 20:40:28 +0000

    Implement first test

--- Tests/Functional/Controller/LoginControllerTest.php
+++ Tests/Functional/Controller/LoginControllerTest.php
@@ -11,8 +11,34 @@ namespace PylonWorks\Essencebase\Tests\Functional\Controller;
  */
 class LoginControllerTest extends \TYPO3\Flow\Tests\FunctionalTestCase {
 
+	/**
+	 * @var boolean
+	 */
+	protected $testableSecurityEnabled = TRUE;
+
+	/**
+	 * @var boolean
+	 */
+	static protected $testablePersistenceEnabled = TRUE;
+
+	/**
+	 * @var string
+	 */
+	protected $userName = 'functional_test_account';
+
+	/**
+	 * @var string
+	 */
+	protected $password = 'a_very_secure_long_password';
+
 	public function setUp() {
 		parent::setUp();
+		$accountRepository = $this->objectManager->get('\TYPO3\Flow\Security\AccountRepository');
+		$accountFactory = $this->objectManager->get('\TYPO3\Flow\Security\AccountFactory');
+
+		$account = $accountFactory->createAccountWithPassword($this->userName, $this->password, array('Administrator'), 'TestingProvider');
+		$accountRepository->add($account);
+		$this->persistenceManager->persistAll();
 
 		$this->registerRoute('loginForm', '', array(
 			'@package' => 'PylonWorks.Essencebase',
@@ -26,6 +52,12 @@ class LoginControllerTest extends \TYPO3\Flow\Tests\FunctionalTestCase {
 			'@action' => 'authenticate',
 			'@format' =>'html'
 		));
+		$this->registerRoute('Call dashboard', 'dashboard', array(
+			'@package' => 'PylonWorks.Essencebase',
+			'@controller' => 'Dashboard',
+			'@action' => 'index',
+			'@format' =>'html'
+		));
 	}
 
 	/**
@@ -35,5 +67,20 @@ class LoginControllerTest extends \TYPO3\Flow\Tests\FunctionalTestCase {
 		$response = $this->browser->request('http://localhost/');
 		$this->assertContains('Please sign in', $response->getContent());
 		$this->assertEquals('200 OK', $response->getStatus());
+	}
+
+	/**
+	 * @test
+	 */
+	public function canSubmitLoginFormAndGetAccessToDashboard() {
+		$this->browser->request('http://localhost/');
+		$loginForm = $this->browser->getForm();
+		$loginForm->setValues(array(
+			'__authentication[TYPO3][Flow][Security][Authentication][Token][UsernamePassword][username]' => $this->userName,
+			'__authentication[TYPO3][Flow][Security][Authentication][Token][UsernamePassword][password]' => $this->password,
+		));
+		$response = $this->browser->submit($loginForm);
+		$this->assertNotEquals('#1222268609: ', $response->getHeader('X-Flow-ExceptionCode'));
+		$this->assertContains("Successfully logged in", $response->getContent());
 	}
 }
