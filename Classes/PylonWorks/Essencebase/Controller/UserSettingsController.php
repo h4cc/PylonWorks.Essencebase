59352d22e1bf6800fb6d075eed0f7168e60d4fdd
Michael Klapper <development@morphodo.com>
2013-01-19 22:00:54 +0000

    Rename "User" into "UserSettings"

---
+++ Classes/PylonWorks/Essencebase/Controller/UserSettingsController.php
@@ -0,0 +1,88 @@
+<?php
+namespace PylonWorks\Essencebase\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "PylonWorks.Essencebase".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * User controller for the PylonWorks.Essencebase package
+ *
+ * @Flow\Scope("singleton")
+ */
+class UserSettingsController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Security\AccountRepository
+	 */
+	protected $accountRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Party\Domain\Repository\PartyRepository
+	 */
+	protected $partyRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Security\Cryptography\HashService
+	 */
+	protected $hashService;
+
+	/**
+	 * @var \TYPO3\Flow\Security\Context
+	 * @Flow\Inject
+	 */
+	protected $securityContext;
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$account = $this->securityContext->getAccount();
+		$this->view->assign('account', $account);
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function initializeUpdateAction() {
+		parent::initializeAction();
+		if ($this->arguments->hasArgument('account')) {
+			$propertyMappingConfigurationForAccount = $this->arguments->getArgument('account')->getPropertyMappingConfiguration();
+			$propertyMappingConfigurationForAccountParty = $propertyMappingConfigurationForAccount->forProperty('party');
+			$propertyMappingConfigurationForAccountPartyName = $propertyMappingConfigurationForAccount->forProperty('party.name');
+			$propertyMappingConfigurationForAccountParty->setTypeConverterOption('TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter', \TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter::CONFIGURATION_TARGET_TYPE, '\TYPO3\Party\Domain\Model\Person');
+			foreach (array($propertyMappingConfigurationForAccountParty, $propertyMappingConfigurationForAccountPartyName) as $propertyMappingConfiguration) {
+				$propertyMappingConfiguration->setTypeConverterOption('TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter', \TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, TRUE);
+				$propertyMappingConfiguration->setTypeConverterOption('TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter', \TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter::CONFIGURATION_MODIFICATION_ALLOWED, TRUE);
+			}
+		}
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Security\Account $account
+	 * @param array $password
+	 * @return void
+	 */
+	public function updateAction(\TYPO3\Flow\Security\Account $account, array $password = array()) {
+		$password = array_shift($password);
+		if (strlen(trim(strval($password))) > 0) {
+			$account->setCredentialsSource($this->hashService->hashPassword($password, 'default'));
+		}
+
+		$this->accountRepository->update($account);
+		$this->partyRepository->update($account->getParty());
+
+		$this->addFlashMessage('The user profile has been updated.');
+		$this->redirect('index');
+	}
+}
+
+?>
