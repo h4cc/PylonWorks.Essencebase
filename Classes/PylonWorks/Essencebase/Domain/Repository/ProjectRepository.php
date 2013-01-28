656a71088da40a76de2893bd1ac2a88b00ff4ccc
Michael Klapper <development@morphodo.com>
2013-01-28 23:04:46 +0000

    Add ProjectController

---
+++ Classes/PylonWorks/Essencebase/Domain/Repository/ProjectRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace PylonWorks\Essencebase\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "PylonWorks.Essencebase".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A repository for Projects
+ *
+ * @Flow\Scope("singleton")
+ */
+class ProjectRepository extends \TYPO3\Flow\Persistence\Repository {
+
+	// add customized methods here
+
+}
+?>
